48e55ffc3ff9f151fce2d1b7def514d91c27d305
Jonathan Yee <webber989@gmail.com>
2018-08-20 18:59:24 -0600

    Cleaned up comments. Adjusted the default ui size

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -40,27 +40,21 @@ void MainWindow::dropEvent(QDropEvent *event)
     //Next is to read the dropped file and make the appropriate updates to the gui.
     QString droppedFilePath(mimeData->text());//text() returns a QString
 
+    //Put the path into a wchar to allow for paths that contain unicode characters
     droppedFilePath = droppedFilePath.replace("file:///", "");
-    //std::string droppedFilePaths = droppedFilePath.toStdString();
+
     wchar_t *path = new wchar_t[droppedFilePath.length() + 1];
     droppedFilePath.toWCharArray(path);
     path[droppedFilePath.length()] = 0;
+
     QFileInfo fi(droppedFilePath);
     //Get the suffix of the file. To determine the encoding type later
     QString ext = fi.suffix();
-    //Convert the QString of the path to the file to a c-string. Taglib only accepts a c-string in its constructor
-    //QByteArray ba = droppedFilePath.toLocal8Bit();
 
-    //const char *c_str2 = ba.data();
-    //droppedFilePath.toStdString().c_str();
-    //ui->lineEditresult->setText(droppedFilePath.toStdString().c_str());
     TagLib::FileRef f(path);
-    //TagLib::FileRef f(QFile::encodeName(droppedFilePath).constData(), true);
-    //ui->lineEditresult->setText(QFile::encodeName(droppedFilePath).constData());
+    //Make sure the file opened actually was opened first
     if(!f.isNull()){
-
         TagLib::Tag *tag = f.tag();
-
         ui->lineEditartist->setText(QString::fromStdString(tag->artist().to8Bit(true)));//to8bit makes sure unicode is displayed
         ui->lineEditname->setText(QString::fromStdString(tag->album().to8Bit(true)));
         //Check if a year of release exists. If it doesnt set the date field to empty
@@ -69,7 +63,6 @@ void MainWindow::dropEvent(QDropEvent *event)
         }else{
             ui->lineEditdate->setText("");
         }
-
         //Check the encoding type for the codec combobox
         if(ext == "mp3"){
             ui->comboBoxcodec->setCurrentIndex(2);
@@ -79,12 +72,11 @@ void MainWindow::dropEvent(QDropEvent *event)
         }
         else if(ext == "m4a"){
             ui->comboBoxcodec->setCurrentIndex(4);
-
         }
 
     }
     else{
-        qDebug() << "Unable to open the file!";
+        //qDebug() << "Unable to open the file!";
     }
     event->acceptProposedAction();
 }
@@ -117,6 +109,9 @@ void MainWindow::on_pushButtonApply_clicked()
     result.append(ui->lineEditartist->text() + " - ");
     result.append(ui->lineEditname->text() + " ");
     result.append("("+ui->lineEditdate->text()+") ");
+    if(ui->lineEditedition->text() != ""){
+        result.append("["+ui->lineEditedition->text()+"]");
+    }
     result.append("["+ui->comboBoxsource->currentText());
     result.append(" "+ui->comboBoxcodec->currentText()+"]");
     ui->lineEditresult->setText(result);
